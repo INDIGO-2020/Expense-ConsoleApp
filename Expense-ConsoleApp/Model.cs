9bb5bdd5dab3c33024077b7008d1175681c96356
Angga <anggaaf007@gmail.com>
2024-10-09 12:42:21 +0700

    first commit

---
+++ Expense-ConsoleApp/Model.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expense_ConsoleApp
+{
+    public class Model
+    {
+        public enum Category
+        {
+            FoodBeverage,
+            Transport,
+            Bill,
+            Fun,
+            Shopping,
+            Education
+        }
+        public Model()
+        {
+            
+        }
+
+        public double Amount { get; set; }
+        public DateTime Date { get; set; }
+        public Category category { get; set; }
+        public string Description { get; set; }
+    }
+}
